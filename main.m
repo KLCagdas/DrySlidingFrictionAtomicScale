6e455606a6c3b57bde00846bc4342a896964e301
Çağdaş Kılıç <61436359+pitorodoptusdaktus@users.noreply.github.com>
2020-08-04 11:18:54 +0300

    Update main.m

--- main.m
+++ main.m
@@ -1,29 +1,30 @@
-variables
+variables   # call the file variables.m 
 
 for n=1:Nstep
   [ztBar, deriv_ztBar] = find_ztBar(lenzt, x, xt, xts, z, zt, cons_LJ, s, ztBars, FN);
   dx = x - xt;   dz = z - ztBar;
-  rhoSqr = (dx.^2 + dz.^2);
-  deriv_LenJones = cons_LJ .* (rhoSqr.^-4 - 2*s^6 * rhoSqr.^-7);
+  rhoSqr = (dx.^2 + dz.^2);   # it is not square root
+  deriv_LenJones = cons_LJ .* (rhoSqr.^-4 - 2*s^6 * rhoSqr.^-7);    # for efficiency, x or z are not included, so that it can be used in all the forces
   
   % force updates
-  Fx = -1 * (dx .* deriv_LenJones + kX * (x - x0) + 2*kpX * x - kX * (xx + x(1, 1:end)));   % try to write xx in a matrix form, but not as a new matrix
+  Fx = -1 * (dx .* deriv_LenJones + kX * (x - x0) + 2*kpX * x - kX * (xx + x(1, 1:end)));
   Fz = -1 * (dz .* deriv_LenJones + kZ * (z - z0));
-  Ftx = -1 * ((sum(dz .* deriv_LenJones) + FN * deriv_ztBar + kT * (xt - xD)));    # check if the derivative is correct in terms of zt_Bar
+  Ftx = -1 * ((sum(dz .* deriv_LenJones) + FN * deriv_ztBar + kT * (xt - xD)));
   Fx(1) = Fx(length(Fx)) = 0;
   Fz(1) = Fz(length(Fz)) = 0;
-  plot(x, z, '*')
+  
+  % plot
+  plot(x, z, 'o', "MarkerSize", 8)
   pause(0.1)
-  
+ 
   % velocity updates
-  vx = vx + Fx * dt/m;   vz = vz + Fz * dt/m;     vtx = vtx + Ftx * dt/mt;
-  
-  % potential of the whole system (VT) update
-  #sys_pot += 4*E*(s^12 / rhoSqr^6 - s^6 / rhoSqr^3) + kT/2 * (xt - xD)^2 + kX/2 * (x - x0)^2 + kpX/2 * ((xx - xx0) - (x - x0))^2 + kZ/2 * (z - z0)^2 + FN * ztBar; 
-  # check whether (xx - x) is in coordination with the article
+  vx = vx + Fx * dt/m;   vz = vz + Fz * dt/m;
+  vxx = vxx + Fx * dt/m;    vtx = vtx + Ftx * dt/mt;        # maybe a special force Fxx should update the velocity vxx
   
   % position updates
-  x = x + vx * dt;   z = z + vz * dt;
+  x = x + vx * dt;   xx = xx + vxx * dt;   z = z + vz * dt;
   xt = xt + vtx * dt;   xts = [xt xts];   xD = xD + vD * dt;
-  sys_pot = 0;
+  
+  % save potential of the whole system(V_T)
+  sys_pot = [sys_pot, sum(4*E* sum(s^12 * rhoSqr.^-6 - s^6 * rhoSqr.^-3)) + kT/2 * (xt - xD).^2 + kX/2 * sum((x - x0).^2) + kpX/2 * ((xx - xx0) - (x - x0)).^2 + kZ/2 * (z - z0).^2 + FN * ztBar];
 end
