6cb864478959d74dc22c497dfac348c4b5c099bc
Çağdaş Kılıç <61436359+pitorodoptusdaktus@users.noreply.github.com>
2020-08-04 11:24:10 +0300

    Update variables.m

--- variables.m
+++ variables.m
@@ -3,10 +3,10 @@ N = 100;
 Nstep = 1000;
 
 % positions
-zi0 = 1;
-x0 = [1:N]*3;   z0 = [zi0 .* ones(1, N)];
-xt = x0(1);   xts = [xt, xt-xt/2];   xD = xt - (x0(1) - x0(2));
-x = x0;   z = z0;  xx0 = [x(1, 3:end), x(1, end)+1, x(1, end)+2];   xx = xx0;    # change the name xx
+x_space = 3;    z_space = 1;    # initialize the space between atoms and the initial height of them 
+x0 = [1:N] * x_space;   z0 = [z_space * ones(1, N)];
+xt = 1.5;   xts = [xt, xt/2];   xD = xt - x_space;
+x = x0;   z = z0;  xx0 = [x(1, 3:end), x(1, end)+ 1*x_space, x(1, end)+ 2*x_space];   xx = xx0;    # 'xx' initializes the variable x_i+1 for the motion equation of x
 zt = [1 2 3 4 5]; 
 global ztBar
 ztBar = 0;
@@ -15,21 +15,21 @@ ztBars = [0];
 
 % velocities
 vx0 = zeros(1, N);    vz0 = zeros(1, N);
-vtx0 = 0;   vD = 4;
+vxx = zeros(1, N);    vtx0 = 0;   vD = 4;
 vx = vx0;   vz = vz0;   vtx = vtx0;
 
 % equations 
 deriv_LenJones = 0; 
 global deriv_ztBar  
 deriv_ztBar = 0;
-sys_pot = 0;
+sys_pot = [];
 
 % attributes
-E = 0.84; s = 2.56;
-kT = 0.2; kX = 4.3; kpX = 5.8; kZ = 10;
+E = 0.84;   s = 2.56;
+kT = 0.2;   kX = 1.7;   kpX = 1.7;    kZ = 10;
 dt = 0.01;
-mt = 0.1; m = 0.1;
-cons_LJ = 24*E*s^6;
+mt = 0.1;   m = 0.1;
+cons_LJ = 24*E*s^6;   # for efficiency
 FN = 0.2;
-lenzt = length(zt);
+lenzt = length(zt);   # for efficiency
 FNs = zeros(1, lenzt);
